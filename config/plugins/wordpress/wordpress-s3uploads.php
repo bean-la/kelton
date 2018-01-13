c2ec4f18130030994da780ecea633e1df456dd32
Sebastian Bean <sebbean@gmail.com>
2018-01-13 12:45:57 -0800

    rearrange plugins. fix initializers

--- config/plugins/wordpress/wordpress-s3uploads.php
+++ config/plugins/wordpress/wordpress-s3uploads.php
@@ -5,13 +5,11 @@
  */
 if (!empty(getenv('AWS_S3_URL'))) {
     $env = parse_url(getenv('AWS_S3_URL'));
-
     define('S3_UPLOADS_AUTOENABLE', true);
     define('S3_UPLOADS_KEY', $env['user']);
     define('S3_UPLOADS_SECRET', $env['pass']);
     define('S3_UPLOADS_REGION', str_replace(array('s3-', '.amazonaws.com'), array('', ''), $env['host']));
     define('S3_UPLOADS_BUCKET', ltrim($env['path'], '/'));
-
     unset($env);
 } else {
     define('S3_UPLOADS_AUTOENABLE', false);
